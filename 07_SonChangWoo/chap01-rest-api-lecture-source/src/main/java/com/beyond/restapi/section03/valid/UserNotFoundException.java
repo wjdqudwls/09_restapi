74e185d9b9a976de1cfbbff6e12b75dbf62bf5bf
jabbit1999 <jabbit1999@gmail.com>
2026-01-06 18:50:41 +0900

    1/6 손창우 강의 내용 업로드

---
+++ 07_SonChangWoo/chap01-rest-api-lecture-source/src/main/java/com/beyond/restapi/section03/valid/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.beyond.restapi.section03.valid;
+
+/* 사용자 정의 예외 */
+public class UserNotFoundException extends Exception {
+
+	public UserNotFoundException(String msg) {
+		super(msg);
+	}
+}
